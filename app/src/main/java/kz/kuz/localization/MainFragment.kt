7c1407496c585f9bbd5c548524991e5ae9dae3ab
Mergayaz <mergayaz@gmail.com>
2023-03-28 22:37:16 +0600

    Project upload

---
+++ app/src/main/java/kz/kuz/localization/MainFragment.kt
@@ -0,0 +1,20 @@
+package kz.kuz.localization
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    // методы фрагмента должны быть открытыми
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        activity?.setTitle(R.string.toolbar_title)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
